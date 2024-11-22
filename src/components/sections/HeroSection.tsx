4f6748ae410aa4f0776941f2a64060c09bf1c6a1
amiteshtripathi12 <manihritik886@gmail.com>
2024-11-22 12:18:43 +0530

    update razorpy link

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -643,7 +643,7 @@ function HeroSection() {
                     <Link
                       href={
                         countryph === "ind"
-                          ? "https://rzp.io/rzp/Y797OIrM"
+                          ? "https://rzp.io/rzp/38770yOK"
                           : "https://www.paypal.com/ncp/payment/S9XLR7J83NHKU"
                       }
                       className="mt-6 bg-primary hover:opacity-90 hover:bg-secondary text-white py-2 px-6 rounded-full shadow-none text-sm sm:text-base lg:text-lg"
