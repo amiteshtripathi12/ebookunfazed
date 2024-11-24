b855191716ad83a1deabd7aa700c678b54a3a475
amiteshtripathi12 <manihritik886@gmail.com>
2024-11-24 14:39:06 +0530

    update paypal link

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -644,7 +644,7 @@ function HeroSection() {
                       href={
                         countryph === "ind"
                           ? "https://rzp.io/rzp/38770yOK"
-                          : "https://www.paypal.com/ncp/payment/W4NWELK8MPZ54"
+                          : "https://rzp.io/rzp/pVu7MXf"
                       }
                       className="mt-6 bg-primary hover:opacity-90 hover:bg-secondary text-white py-2 px-6 rounded-full shadow-none text-sm sm:text-base lg:text-lg"
                     >
