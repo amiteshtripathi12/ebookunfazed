d3bb97026dba2545c7ff7314ce9dbafe8c454832
amiteshtripathi12 <manihritik886@gmail.com>
2024-12-08 19:33:24 +0530

    code update

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -236,7 +236,7 @@ function HeroSection() {
 
   const handlePayment = async () => {
     if (selectedCard == 1 && countryph === "ind") {
-      router.push("https://rzp.io/rzp/rqKi68A "); //1998 test
+      router.push("https://rzp.io/rzp/rqKi68A"); //1998 test
     } else if (selectedCard == 0 && countryph === "ind") {
       router.push("https://rzp.io/rzp/38770yOK"); //
     } else if (selectedCard == 0 && countryph === "us") {
