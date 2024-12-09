7ab9cdf177378b3bcc01df97aa8ade6a7f5b6498
“Yogesh <yogeshprajapati8989@gmail.com>
2024-12-09 14:18:23 +0530

    changes

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -593,6 +593,9 @@ function HeroSection() {
                 <div className=" max-h-[75vh] overflow-y-auto custom-scrollbar">
                   <div className="flex flex-col items-center justify-center rounded-lg  w-full mx-auto ">
                     <div className=" max-w-md mt-6">
+                      <span className="ml-2 mb-2">
+                        APPLY THE COUPON AND SEE THE MAGIC UNFOLD!!
+                      </span>
                       <div className="flex gap-2 mb-2">
                         <Input
                           placeholder="Enter coupon code"
@@ -660,7 +663,7 @@ function HeroSection() {
                               >
                                 {card.isPopular && (
                                   <div className="uppercase bg-gradient-to-r from-primary to-secondary rounded-t-2xl p-3 text-center text-white">
-                                    MOST POPULAR
+                                    ONE TIME OFFER
                                   </div>
                                 )}
 
@@ -689,6 +692,29 @@ function HeroSection() {
                                     )}
                                   </div>
                                   <ul className="mb-4 space-y-2 text-left text-lg">
+                                    <li className="flex justify-between items-start space-x-4 border-b pt-2 mt-2">
+                                      <span className="ml-10">Bonus</span>
+                                      <span className="text-gray-900 text-lg">
+                                        Worth
+                                      </span>
+                                    </li>
+                                    <li className="flex items-center space-x-4">
+                                      <svg
+                                        className="flex-shrink-0 w-6 h-6 text-green-400"
+                                        viewBox="0 0 30 30"
+                                        fill="none"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                      >
+                                        <path
+                                          d="M10 14.7875L13.0959 17.8834C13.3399 18.1274 13.7353 18.1275 13.9794 17.8838L20.625 11.25M15 27.5C8.09644 27.5 2.5 21.9036 2.5 15C2.5 8.09644 8.09644 2.5 15 2.5C21.9036 2.5 27.5 8.09644 27.5 15C27.5 21.9036 21.9036 27.5 15 27.5Z"
+                                          stroke="currentColor"
+                                          strokeWidth="1.6"
+                                          strokeLinecap="round"
+                                          strokeLinejoin="round"
+                                        />
+                                      </svg>
+                                      <span>EBook</span>
+                                    </li>
                                     {card.features.map((feature, index) => (
                                       <li
                                         key={index}
@@ -742,6 +768,7 @@ function HeroSection() {
                                         </span>
                                       </li>
                                     ))}
+
                                     <li className="flex justify-between items-start space-x-4 border-t pt-2 mt-2">
                                       <span className="font-bold ml-12">
                                         Total
@@ -752,6 +779,18 @@ function HeroSection() {
                                           : card.price}
                                       </span>
                                     </li>
+                                    {isApplied && (
+                                      <li className="flex justify-between items-start space-x-4 border-t pt-2 mt-2">
+                                        <span className="font-bold ml-12">
+                                          At just
+                                        </span>
+                                        <span className="text-gray-900 font-semibold text-lg">
+                                          {countryph === "ind"
+                                            ? card.priceDiscountedINR
+                                            : card.priceDiscounted}
+                                        </span>
+                                      </li>
+                                    )}
                                   </ul>
 
                                   <button
